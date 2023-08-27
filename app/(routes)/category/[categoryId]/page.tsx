8da240cbf5ec0628c14269b082bbbb125bc40ea1
Adetayo <akinsanyaadetayo@gmail.com>
2023-08-27 05:48:26 +0100

    Add webhook

--- app/(routes)/category/[categoryId]/page.tsx
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -1,10 +1,9 @@
-
 import Container from '@/components/ui/container';
 import Billboard from '@/components/ui/billboard';
 import ProductCard from '@/components/ui/product-card';
 import NoResults from '@/components/ui/no-results';
 
-import getProducts from "@/actions/get-products";
+import getProducts from '@/actions/get-products';
 import getCategory from '@/actions/get-category';
 import getSizes from '@/actions/get-sizes';
 import getColors from '@/actions/get-colors';
@@ -17,18 +16,18 @@ export const revalidate = 0;
 interface CategoryPageProps {
   params: {
     categoryId: string;
-  },
+  };
   searchParams: {
     colorId: string;
     sizeId: string;
-  }
+  };
 }
 
-const CategoryPage: React.FC<CategoryPageProps> = async ({ 
-  params, 
-  searchParams
+const CategoryPage: React.FC<CategoryPageProps> = async ({
+  params,
+  searchParams,
 }) => {
-  const products = await getProducts({ 
+  const products = await getProducts({
     categoryId: params.categoryId,
     colorId: searchParams.colorId,
     sizeId: searchParams.sizeId,
@@ -37,30 +36,22 @@ const CategoryPage: React.FC<CategoryPageProps> = async ({
   const colors = await getColors();
   const category = await getCategory(params.categoryId);
 
+  console.log(category.billboard);
+
   return (
     <div className="bg-white">
       <Container>
-        <Billboard 
-          data={category.billboard}
-        />
-        <div className="px-4 sm:px-6 lg:px-8 pb-24">
+        <Billboard data={category.billboard} />
+        <div className="px-4 pb-24 sm:px-6 lg:px-8">
           <div className="lg:grid lg:grid-cols-5 lg:gap-x-8">
             <MobileFilters sizes={sizes} colors={colors} />
             <div className="hidden lg:block">
-              <Filter
-                valueKey="sizeId" 
-                name="Sizes" 
-                data={sizes}
-              />
-              <Filter 
-                valueKey="colorId" 
-                name="Colors" 
-                data={colors}
-              />
+              <Filter valueKey="sizeId" name="Sizes" data={sizes} />
+              <Filter valueKey="colorId" name="Colors" data={colors} />
             </div>
             <div className="mt-6 lg:col-span-4 lg:mt-0">
               {products.length === 0 && <NoResults />}
-              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+              <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3">
                 {products.map((item) => (
                   <ProductCard key={item.id} data={item} />
                 ))}
--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,27 +1,24 @@
-import getBillboard from "@/actions/get-billboard";
-import getProducts from "@/actions/get-products";
-import ProductList from "@/components/product-list";
-import Billboard from "@/components/ui/billboard";
-import Container from "@/components/ui/container";
+import getBillboard from '@/actions/get-billboard';
+import getProducts from '@/actions/get-products';
+import ProductList from '@/components/product-list';
+import Billboard from '@/components/ui/billboard';
+import Container from '@/components/ui/container';
 
 export const revalidate = 0;
 
 const HomePage = async () => {
   const products = await getProducts({ isFeatured: true });
-  const billboard = await getBillboard("0e12e5cf-29ab-4529-b8d5-c5371dae1f7b");
-
+  const billboard = await getBillboard('790e27b0-0fa0-4f65-a947-922459463c52');
   return (
     <Container>
-      <div className="space-y-10 pb-10">
-        <Billboard 
-          data={billboard}
-        />
-        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+      <div className="pb-10 space-y-10">
+        <Billboard data={billboard} />
+        <div className="flex flex-col px-4 gap-y-8 sm:px-6 lg:px-8">
           <ProductList title="Featured Products" items={products} />
         </div>
       </div>
     </Container>
-  )
+  );
 };
 
 export default HomePage;
